e6003c7a6870171e9637f33ae9332fde96f81181
Vladislav Marinichev <vladislav-marinichev@rambler.ru>
2020-06-08 00:13:26 +0300

    stat service v.1

--- stackoverflow_stats.py
+++ stackoverflow_stats.py
@@ -1,8 +1,39 @@
 from flask import Flask
+import json
 
+import requests
 app = Flask(__name__)
 
+@app.route('/health')
+def health():
+    return 'Application is running...'
 
-@app.route('/')
-def home():
-    return 'Hello, world BRO!'
+@app.route('/search/<tag>')
+def get_cur_time(tag):
+    result = {}
+
+    result[tag] = rec(tag)
+
+    return json.dumps(result)
+
+
+def request_service(tag, page):
+    payload = {'page': page, 'pagesize': 100, 'tagged': tag, 'site': 'stackoverflow', 'order': 'desc', 'sort': 'creation'}
+    url = 'https://api.stackexchange.com/search'
+    response = requests.get(url, params=payload)
+    return response.json()
+
+
+def rec(tag, total=0, answered=0, page=1, has_more=True):
+    if has_more:
+       rj = request_service(tag, page)
+
+       total += len(rj['items'])
+       answered += len(list(filter(lambda x: x['is_answered'], rj['items'])))
+
+       rec(tag, total, answered, page + 1, rj['has_more'])
+    else:
+        stat_dict = {}
+        stat_dict['total'] = total
+        stat_dict['answered'] = answered
+        return stat_dict
